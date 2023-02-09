f9b556197f865ff449fef0173949ca1afc25669c
Vít Vosol <vosol@SPSEJECNANET.CZ>
2023-02-09 09:02:55 +0100

    final commit

--- script.js
+++ script.js
@@ -24,26 +24,34 @@ class Auto {
 const list = [];
 let autoservis = new Autoservis("Autoservis", list);
 
-let a1 = new Auto("skoda", 1560,"2004", "felicia")
-let a2 = new Auto("skoda", 1560,"2004", "felicia")
-let a3 = new Auto("skoda", 1560,"2004", "felicia")
+let a1 = new Auto("skoda", 1560, "2004", "felicia")
+let a2 = new Auto("skoda", 1560, "2004", "felicia")
+let a3 = new Auto("skoda", 1560, "2004", "felicia")
 
 autoservis.addAuto(a1);
 autoservis.addAuto(a2);
 autoservis.addAuto(a3);
 
-const selectAuto = document.getElementById("autoD");
-
 const ot1 = document.getElementById("vsechnaAuta");
 const ot2 = document.getElementById("deleteA");
 
 ot1.addEventListener("click", () => {
+    vypis.textContent = "";
     autoservis.list.forEach(Auto => {
-        vypis.textContent += "Znacka: " + Auto.znacka + ", najete kilometry: " + Auto.najeteKilometry + " rok vyroby: " + Auto.rokVzniku + ", model: " + Auto.model + " \n"; 
+        vypis.textContent += "Znacka: " + Auto.znacka + ", najete kilometry: " + Auto.najeteKilometry + " rok vyroby: " + Auto.rokVzniku + ", model: " + Auto.model + " \n";
     })
 });
 ot2.addEventListener("click", () => {
-    
+    if (document.getElementById("autoD").value != null) {
+        var i = 0;
+        while (i < autoservis.list.length) {
+            if (autoservis.list[i].nazev === document.getElementById("autoD").value) {
+                autoservis.list.splice(i, 1);
+            } else {
+                ++i;
+            }
+        }
+    }
 })
 
 
